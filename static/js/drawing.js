7dcb0248d203abf40423b1d902bfe633a32c09fc
aanikina <AAnikina@uclan.ac.uk>
2022-04-11 14:12:41 +0300

    touch input test4

--- static/js/drawing.js
+++ static/js/drawing.js
@@ -14,44 +14,74 @@ var x = "black",
 
 function init() {
 
-    canvas = document.getElementById('can');
-    ctx = canvas.getContext("2d");
-    w = canvas.width;
-    h = canvas.height;
+    canvas=document.getElementById( 'can' );
+    ctx=canvas.getContext( "2d" );
+    w=canvas.width;
+    h=canvas.height;
 
     //console.log( `width: ${w} height: ${h}` );
 
-    canvas.addEventListener("mousemove", function (e) {
-        findxy('move', e)
-    }, false);
-    canvas.addEventListener("mousedown", function (e) {
-        findxy('down', e)
-    }, false);
-    canvas.addEventListener("mouseup", function (e) {
-        findxy('up', e)
-    }, false);
-    canvas.addEventListener("mouseout", function (e) {
-        findxy('out', e)
-    }, false);
+    canvas.addEventListener( "mousemove", function( e ) {
+        findxy( 'move', e )
+    }, false );
+    canvas.addEventListener( "mousedown", function( e ) {
+        findxy( 'down', e )
+    }, false );
+    canvas.addEventListener( "mouseup", function( e ) {
+        findxy( 'up', e )
+    }, false );
+    canvas.addEventListener( "mouseout", function( e ) {
+        findxy( 'out', e )
+    }, false );
 
     // The code for the following event listeners that
-    // prevent scrolling when touching the canvas
+    // handle touch interaction
     // is appropriated from publicly available snippet which can be found here:
     // https://bencentra.com/code/2014/12/05/html5-canvas-touch-events.html
+
+    canvas.addEventListener( "touchstart", function( e ) {
+        mousePos=getTouchPos( canvas, e );
+        var touch=e.touches[ 0 ];
+        var mouseEvent=new MouseEvent( "mousedown", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        } );
+        canvas.dispatchEvent( mouseEvent );
+    }, false );
+    canvas.addEventListener( "touchend", function( e ) {
+        var mouseEvent=new MouseEvent( "mouseup", {} );
+        canvas.dispatchEvent( mouseEvent );
+    }, false );
+    canvas.addEventListener( "touchmove", function( e ) {
+        var touch=e.touches[ 0 ];
+        var mouseEvent=new MouseEvent( "mousemove", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        } );
+        canvas.dispatchEvent( mouseEvent );
+    }, false );
+
+    // Get the position of a touch relative to the canvas
+    function getTouchPos( canvasDom, touchEvent ) {
+        var rect=canvasDom.getBoundingClientRect();
+        return {
+            x: touchEvent.touches[ 0 ].clientX-rect.left,
+            y: touchEvent.touches[ 0 ].clientY-rect.top
+        };
+    }
+
+    // Prevent scrolling when touching the canvas
     document.body.addEventListener("touchstart", function (e) {
-        findxy('up', e);
         if (e.target == canvas) {
             e.preventDefault();
         }
     }, false);
     document.body.addEventListener("touchend", function (e) {
-        findxy('down', e);
         if (e.target == canvas) {
             e.preventDefault();
         }
     }, false);
     document.body.addEventListener("touchmove", function (e) {
-        findxy('move', e);
         if (e.target == canvas) {
             e.preventDefault();
         }
